49dad05ab7faabb15d0e3381583918afed0000b3
Armon Dadgar <armon.dadgar@gmail.com>
2014-06-18 17:36:33 -0700

    Working on replication

--- main.go
+++ main.go
@@ -5,20 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/armon/consul-api"
 )
 
 type ReplicationConfig struct {
-	SourceDC      string
-	DestinationDC string
+	Name string
+	Pid  int
+
+	SourceDC string
 
 	SourcePrefix      string
 	DestinationPrefix string
 
 	Lock    string
+	Status  string
 	Service string
 }
 
@@ -31,11 +37,11 @@ func realMain() int {
 	replConf := &ReplicationConfig{}
 	flag.Usage = usage
 	flag.StringVar(&replConf.SourceDC, "src", "", "source datacenter")
-	flag.StringVar(&replConf.DestinationDC, "dst", "", "destination datacenter, defaults to local")
 	flag.StringVar(&replConf.SourcePrefix, "prefix", "global/", "source prefix")
 	flag.StringVar(&replConf.DestinationPrefix, "dst-prefix", "", "destination prefix, defaults to source prefix")
 	flag.StringVar(&consulConf.Address, "addr", "127.0.0.1:8500", "consul HTTP API address with port")
 	flag.StringVar(&replConf.Lock, "lock", "service/consul-replicate/leader", "Lock used for coordination")
+	flag.StringVar(&replConf.Status, "status", "service/consul-replicate/status", "Status file used for state")
 	flag.StringVar(&replConf.Service, "service", "consul-replicate", "Service used for registration")
 	flag.Parse()
 
@@ -58,25 +64,52 @@ func realMain() int {
 		log.Printf("[ERR] Failed to query agent: %v", err)
 		return 1
 	}
+	localDC := info["Config"]["Datacenter"].(string)
+
+	// Set our name and pid
+	replConf.Name = info["Config"]["NodeName"].(string)
+	replConf.Pid = syscall.Getpid()
 
 	// Fill in the defaults
 	if replConf.DestinationPrefix == "" {
 		replConf.DestinationPrefix = replConf.SourcePrefix
 	}
-	if replConf.DestinationDC == "" {
-		replConf.DestinationDC = info["Config"]["Datacenter"].(string)
-	}
 
 	// Sanity check config
-	if replConf.SourceDC == replConf.DestinationDC {
-		log.Printf("[ERR] Destination DC cannot be the source DC")
+	if replConf.SourceDC == localDC {
+		log.Printf("[ERR] Local DC cannot be the source DC")
 		return 1
 	}
 
 	// Log what we are about to do
 	log.Printf("[INFO] Attempting to replicate from DC %s (%s) to %s (%s)",
 		replConf.SourceDC, replConf.SourcePrefix,
-		replConf.DestinationDC, replConf.DestinationPrefix)
+		localDC, replConf.DestinationPrefix)
+
+	// Start replication
+	stopCh := make(chan struct{})
+	doneCh := make(chan struct{})
+	go replicate(replConf, client, stopCh, doneCh)
+
+	// Wait for termination
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	select {
+	case <-signalCh:
+		close(stopCh)
+		log.Printf("[WARN] Received signal, stopping replication")
+	case <-doneCh:
+		close(stopCh)
+		log.Printf("[WARN] Replication terminated")
+		return 1
+	}
+
+	// Wait for clean termination, or timeout
+	select {
+	case <-doneCh:
+	case <-time.After(5 * time.Second):
+		log.Printf("[WARN] Timed out waiting for replication to stop")
+	}
 	return 0
 }
 
@@ -88,23 +121,24 @@ func usage() {
 const helpText = `
 Usage: %s [options]
 
-  Replicates K/V data from a source datacenter to a target
-  datacenter.
+  Replicates K/V data from a source datacenter to the datacenter of
+  a Consul agent.
 
 Options:
 
   -addr=127.0.0.1:8500  Provides the HTTP address of a Consul agent.
-  -src=dc1              Provides the source destination to replicate from
-  -dst=dc2              Provides the destination datacenter. Defaults to the
-                        datacenter of the agent being used.
-  -prefix=global/       Provides the prefix which is the root of replicated keys
-                        in the source datacenter
   -dst-prefix=global/   Provides the prefix which is the root of replicated keys
                         in the destination datacenter. Defaults to match source.
   -lock=path            Lock is used to provide the path in the KV store used to
                         perform leader election for the replicators. This ensures
                         a single replicator running per-DC in a high-availability
                         setup. Defaults to "service/consul-replicate/leader"
+  -prefix=global/       Provides the prefix which is the root of replicated keys
+                        in the source datacenter
   -service              Service sets the name of the service that is registered
                         in the catalog. Defaults to "consul-replicate"
+  -src=dc1              Provides the source destination to replicate from
+  -status=path         Status is used to provide the path in the KV store used to
+                        store our replication status. This is to checkpoint replication
+						periodically. Defaults to "service/consul-replicate/status"
 `
---
+++ replicate.go
@@ -0,0 +1,276 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/armon/consul-api"
+	"log"
+	"time"
+)
+
+const (
+	// statusCheckpoint controls how often we write back out status
+	statusCheckpoint = 5 * time.Second
+)
+
+// status struct is used to checkpoint our status
+type status struct {
+	LastReplicated uint64
+}
+
+// shouldQuit is used to check if a stop channel is closed
+func shouldQuit(stopCh chan struct{}) bool {
+	select {
+	case <-stopCh:
+		return true
+	default:
+		return false
+	}
+}
+
+// replicate is a long running routine that manages replication.
+// The stopCh is used to signal we should terminate, and the doneCh is closed
+// when we finish
+func replicate(conf *ReplicationConfig, client *consulapi.Client, stopCh, doneCh chan struct{}) {
+	defer func() {
+		// Cleanup the service entry
+		if err := removeService(conf, client); err != nil {
+			log.Printf("[ERR] Failed to cleanup %s service: %v", conf.Service, err)
+		}
+		close(doneCh)
+	}()
+
+	// Ensure the service is setup on the local agent
+	if err := setupService(conf, client); err != nil {
+		log.Printf("[ERR] Failed to setup %s service: %v", conf.Service, err)
+		return
+	}
+
+	// Keep our health check alive
+	if err := maintainCheck(conf, client, doneCh); err != nil {
+		log.Printf("[ERR] Failed to update check TTL: %v", err)
+		return
+	}
+
+ACQUIRE:
+	// Re-check if we should exit
+	if shouldQuit(stopCh) {
+		return
+	}
+
+	// Acquire leadership for this
+	leaderCh, err := acquireLeadership(conf, client, stopCh)
+	if err != nil {
+		log.Printf("[ERR] Failed to acquire leadership: %v", err)
+		return
+	}
+
+	// Replicate now that we are the leader
+	if err := replicateKeys(conf, client, leaderCh, stopCh); err != nil {
+		log.Printf("[ERR] Failed to replicate keys: %v", err)
+	}
+	goto ACQUIRE
+}
+
+// serviceID generates an ID for the service
+func serviceID(conf *ReplicationConfig) string {
+	return fmt.Sprintf("%s-%d", conf.Service, conf.Pid)
+}
+
+// checkID generates an ID for the service check
+func checkID(conf *ReplicationConfig) string {
+	return fmt.Sprintf("service:%s", serviceID(conf))
+}
+
+// setupService is used to create the local service entries with the agent
+func setupService(conf *ReplicationConfig, client *consulapi.Client) error {
+	reg := &consulapi.AgentServiceRegistration{
+		ID:   serviceID(conf),
+		Name: conf.Service,
+		Check: &consulapi.AgentServiceCheck{
+			TTL: "5s",
+		},
+	}
+	agent := client.Agent()
+	return agent.ServiceRegister(reg)
+}
+
+// removeService is used to cleanup the service entry we created
+func removeService(conf *ReplicationConfig, client *consulapi.Client) error {
+	agent := client.Agent()
+	return agent.ServiceDeregister(serviceID(conf))
+}
+
+// maintainCheck periodically hits our TTL check to keep it alive
+func maintainCheck(conf *ReplicationConfig, client *consulapi.Client, doneCh chan struct{}) error {
+	// Try to update the TTL immediately
+	agent := client.Agent()
+	checkID := checkID(conf)
+	if err := agent.PassTTL(checkID, ""); err != nil {
+		return err
+	}
+
+	// Run the update in the background
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				if err := agent.PassTTL(checkID, ""); err != nil {
+					log.Printf("[ERR] Failed to update check TTL: %v", err)
+				}
+			case <-doneCh:
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+// lockValue generates a value to set for the key lock
+func lockValue(conf *ReplicationConfig) []byte {
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.Encode(conf)
+	return buf.Bytes()
+}
+
+// acquireLeadership is used to wait until we are the leader in the cluster
+func acquireLeadership(conf *ReplicationConfig, client *consulapi.Client, stopCh chan struct{}) (chan struct{}, error) {
+	// Create a new session
+	kv := client.KV()
+	session := client.Session()
+	se := &consulapi.SessionEntry{
+		Name:   fmt.Sprintf("Lock for %s service", conf.Service),
+		Checks: []string{checkID(conf), "serfHealth"},
+	}
+	id, _, err := session.Create(se, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct a key to lock
+	p := &consulapi.KVPair{
+		Key:     conf.Lock,
+		Value:   lockValue(conf),
+		Session: id,
+	}
+
+	opts := &consulapi.QueryOptions{
+		WaitTime: 60 * time.Second,
+	}
+WAIT:
+	if shouldQuit(stopCh) {
+		return nil, fmt.Errorf("Lock acquisition aborted")
+	}
+
+	// Look for an existing lock, blocking until not taken
+	pair, meta, err := kv.Get(conf.Lock, opts)
+	if err != nil {
+		return nil, err
+	}
+	if pair != nil && pair.Session != "" {
+		opts.WaitIndex = meta.LastIndex
+		goto WAIT
+	}
+
+	// Try to acquire the lock
+	locked, _, err := kv.Acquire(p, nil)
+	if err != nil {
+		return nil, err
+	}
+	if !locked {
+		log.Printf("[WARN] Failed to acquire lock on %s, sleeping", conf.Lock)
+		select {
+		case <-time.After(5 * time.Second):
+			goto WAIT
+		case <-stopCh:
+			return nil, fmt.Errorf("Lock acquisition aborted")
+		}
+	}
+
+	// Watch to ensure we maintain leadership
+	leaderCh := make(chan struct{})
+	go watchLock(conf, client, id, leaderCh)
+
+	// Locked! All done
+	log.Printf("[INFO] Lock %s acquired", conf.Lock)
+	return leaderCh, nil
+}
+
+// watchLock watches the given key to ensure we are still the leader
+func watchLock(conf *ReplicationConfig, client *consulapi.Client, session string, stopCh chan struct{}) {
+	kv := client.KV()
+	opts := &consulapi.QueryOptions{}
+WAIT:
+	pair, meta, err := kv.Get(conf.Lock, opts)
+	if err != nil {
+		close(stopCh)
+		log.Printf("[ERR] Stepping down, failed to watch lock: %v", err)
+		return
+	}
+	if pair != nil && pair.Session == session {
+		opts.WaitIndex = meta.LastIndex
+		goto WAIT
+	}
+	close(stopCh)
+	log.Printf("[ERR] Stepping down, session invalidated")
+	return
+}
+
+// replicateKeys is used to actively replicate once we are the leader
+func replicateKeys(conf *ReplicationConfig, client *consulapi.Client, leaderCh, stopCh chan struct{}) error {
+	// Read our last status
+	status, err := readStatus(conf, client)
+	if err != nil {
+		return fmt.Errorf("failed to read replication status: %v", err)
+	}
+
+	for {
+		select {
+		case <-time.After(statusCheckpoint):
+			if err := writeStatus(conf, client, status); err != nil {
+				log.Printf("[ERR] Failed to checkpoint replication status: %v", err)
+			}
+		case <-leaderCh:
+			return nil
+		case <-stopCh:
+			return nil
+		}
+	}
+}
+
+// readStatus is used to read the last replication status
+func readStatus(conf *ReplicationConfig, client *consulapi.Client) (*status, error) {
+	kv := client.KV()
+	pair, _, err := kv.Get(conf.Status, nil)
+	if err != nil {
+		return nil, err
+	}
+	status := new(status)
+	if pair != nil {
+		dec := json.NewDecoder(bytes.NewReader(pair.Value))
+		if err := dec.Decode(status); err != nil {
+			return nil, err
+		}
+	}
+	return status, nil
+}
+
+// writeStatus is used to update the last replication status
+func writeStatus(conf *ReplicationConfig, client *consulapi.Client, status *status) error {
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.Encode(status)
+
+	// Create the KVPair
+	pair := &consulapi.KVPair{
+		Key:   conf.Status,
+		Value: buf.Bytes(),
+	}
+	kv := client.KV()
+	_, err := kv.Put(pair, nil)
+	return err
+}
